e1d76673cf9847e928ec84281f1fa83e71532d3b
matheusgarcia3346 <matheusgarcia3346@gmail.com>
2025-04-09 22:48:28 +0000

    Criação do Java Script, da função para receber o valor da conta e o número de pessoas.

---
+++ main.js
@@ -0,0 +1,17 @@
+let bill = 0
+let tipPercentage = 0
+let numberOfPeople = 0
+let buttonSelected = null
+
+function receiveBillValue() {
+    bill = document.querySelector("#bill").valueAsNumber
+}
+
+function receiveNumberOfPeopleValue(){
+    
+    numberOfPeople = document.querySelector("#people").valueAsNumber
+}
+
+function receiveTipPercentageValue(value) {
+
+}
